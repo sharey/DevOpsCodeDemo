efe15544f475682eeba9f1753a08d965e93c77e8
Sharey <shareyjust4u@gmail.com>
2023-01-07 11:24:54 +0530

    Update AddressbookUI.java

--- src/main/java/com/mydemo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/mydemo/tutorial/addressbook/AddressbookUI.java
@@ -71,7 +71,7 @@ public class AddressbookUI extends UI {
          */
         newContact.addClickListener(e -> contactForm.edit(new Contact()));
 
-        filter.setInputPrompt("Filter contacts here...");
+        filter.setInputPrompt("Filter Contacts here...");
         filter.addTextChangeListener(e -> refreshContacts(e.getText()));
 
         contactList
